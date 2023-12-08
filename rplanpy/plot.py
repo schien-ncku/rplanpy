75196ade03597d94e774ddbc18fa1f0184c2bcb5
Nik Chien <52667749+schien-ncku@users.noreply.github.com>
2023-12-09 04:51:37 +0800

    Update plot.py

--- rplanpy/plot.py
+++ rplanpy/plot.py
@@ -65,7 +65,7 @@ def plot_floorplan_graph(data: data.RplanData, ax=None, title=None,
     edge_widths = [int(G.edges[n]['door'])*5+0.5 for n in G.edges]
     if with_colors:
         colors = [np.array(utils.ROOM_COLOR.get(G.nodes[n]['category'], [255, 255, 255]))/255 for n in G.nodes]
-        nx.draw(G, pos, with_labels=False, ax=ax, node_color=colors, edgecolors=[0.2, 0.2, 0.2], node_size=sizes, width=edge_widths, edge_color=[0,0,0,0.4])
+        nx.draw(G, pos, with_labels=False, ax=ax, node_color=colors, edgecolors=[0.2, 0.2, 0.2], node_size=sizes, width=edge_widths)
     else:
         nx.draw(G, pos, with_labels=True, ax=ax, node_size=sizes, width=edge_widths)
     if edge_label:
